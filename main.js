65f4f25ff69b80f25358f24f90a5cd61278ed08d
Selin145 <110917260+Selin145@users.noreply.github.com>
2024-07-23 10:47:31 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 song = "";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function setup(){
-    canvas= createCanvas(600, 500);
+    canvas= createCanvas(700, 600);
     canvas.center();
 
     video=createCapture(VIDEO);
     video.hide();
-}
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);}
 function draw(){
     image (video,0,0,600,500);
 }
@@ -15,4 +20,21 @@ function prelaod(){
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+
+
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results){
+    if(results.lenght>0)
+    {
+        
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWrist = " + rightWristX + "rightWristY  = "+ rightWristY);
+    }
 }
